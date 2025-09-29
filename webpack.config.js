c56a2499c3e0076ad511df084f46a7e196d35fc3
Vladimir <vl.jw@yandex.ru>
2025-09-30 00:31:21 +1000

    Initial commit

---
+++ webpack.config.js
@@ -0,0 +1,69 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+
+module.exports = (env, argv) => {
+  const mode = argv.mode || 'development';
+  const isDev = mode !== 'production';
+
+  return {
+    mode,
+    entry: path.resolve(__dirname, 'src', 'index.tsx'),
+    output: {
+      path: path.resolve(__dirname, 'dist'),
+      filename: isDev ? 'js/[name].js' : 'js/[name].[contenthash:8].js',
+      assetModuleFilename: 'assets/[name][hash][ext][query]',
+      clean: true
+    },
+    devtool: isDev ? 'eval-cheap-module-source-map' : 'source-map',
+    resolve: {
+      extensions: ['.ts', '.tsx', '.js', '.jsx'],
+      alias: { '@': path.resolve(__dirname, 'src') }
+    },
+    module: {
+      rules: [
+        { test: /\.[jt]sx?$/, use: 'ts-loader', exclude: /node_modules/ },
+        {
+          test: /\.module\.s[ac]ss$/,
+          use: [
+            isDev ? 'style-loader' : MiniCssExtractPlugin.loader,
+            {
+              loader: 'css-loader',
+              options: {
+                modules: { localIdentName: isDev ? '[local]_[hash:base64:5]' : '[hash:base64:7]' },
+                importLoaders: 2
+              }
+            },
+            'postcss-loader',
+            'sass-loader'
+          ]
+        },
+        {
+          test: /\.s[ac]ss$/,
+          exclude: /\.module\.s[ac]ss$/,
+          use: [
+            isDev ? 'style-loader' : MiniCssExtractPlugin.loader,
+            { loader: 'css-loader', options: { importLoaders: 2 } },
+            'postcss-loader',
+            'sass-loader'
+          ]
+        },
+        { test: /\.css$/, use: [isDev ? 'style-loader' : MiniCssExtractPlugin.loader, 'css-loader', 'postcss-loader'] },
+        { test: /\.(png|jpe?g|gif|svg|webp|ico)$/i, type: 'asset/resource' },
+        { test: /\.(woff2?|eot|ttf|otf)$/i, type: 'asset/resource' }
+      ]
+    },
+    plugins: [
+      new HtmlWebpackPlugin({ template: path.resolve(__dirname, 'public', 'index.html') }),
+      new MiniCssExtractPlugin({ filename: isDev ? 'css/[name].css' : 'css/[name].[contenthash:8].css' })
+    ],
+    devServer: {
+      static: path.resolve(__dirname, 'public'),
+      port: 5173,
+      hot: true,
+      open: true,
+      historyApiFallback: true
+    },
+    performance: { hints: false }
+  };
+};
